b0b4139b538feacf380b5f004a93871327315d73
Ronald <ronnieadewoye@gmail.com>
2025-06-10 19:19:43 +0100

    ..

--- lib/examples/Reusable2.dart
+++ lib/examples/Reusable2.dart
@@ -23,45 +23,51 @@ class BrandCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         border: Border.all(color: Colors.grey.shade300),
       ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.asset(
-            logoPath,
-            height: 24,
-            width: 24,
-            fit: BoxFit.contain,
-          ),
-          SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
+            // mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
+              Image.asset(
+                logoPath,
+                height: 40,
+                width: 30,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Row(
+                    children: [
+                      Text(
+                        brandName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Icon(
+                        Icons.verified,
+                        size: 16,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 2),
                   Text(
-                    brandName,
+                    '$productCount produ..',
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
+                      color: Colors.grey.shade600,
+                      fontSize: 12,
                     ),
                   ),
-                  SizedBox(width: 4),
-                  Icon(
-                    Icons.verified,
-                    size: 16,
-                    color: Colors.blue,
-                  ),
                 ],
-              ),
-              SizedBox(height: 2),
-              Text(
-                '$productCount produ..',
-                style: TextStyle(
-                  color: Colors.grey.shade600,
-                  fontSize: 12,
-                ),
               ),
             ],
           ),
